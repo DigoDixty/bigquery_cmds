c8e05822fe708547422fce037152c513be198ddb
Rodrigo Cardoso <digo.bigdata@outlook.com>
2022-05-12 19:54:41 -0300

    Create table_partitioned.sql

---
+++ table_partitioned.sql
@@ -0,0 +1,12 @@
+CREATE TABLE if not exists
+  mydataset.newtable (transaction_id INT64, transaction_date DATE)
+PARTITION BY
+  transaction_date
+OPTIONS(
+  partition_expiration_days=3,
+  require_partition_filter=true
+);
+
+select * from mydataset.newtable where transaction_date = current_date();
+
+drop table if exists mydataset.newtable;
